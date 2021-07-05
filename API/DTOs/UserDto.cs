b6cf01f5e96e8ff03815c78ec3ac2703886d656d
Chien <chienndsl@gmail.com>
2021-07-05 17:38:25 +0700

    End of section 11

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -3,8 +3,10 @@ namespace API.DTOs
     public class UserDto
     {
         public string UserName { get; set; }
-
         public string Token { get; set; }
-      
+
+        public string PhotoUrl { get; set; }
+        
+        
     }
 }
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -22,23 +22,25 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private accountService: AccountService, private memberSecvice: MembersService,
+  constructor(private accountService: AccountService, private memberService: MembersService,
      private toastr:ToastrService) { 
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
   } 
 
   ngOnInit(): void {
   this.loadMember();
+
   }
 
   loadMember(){
-    this.memberSecvice.getMember(this.user.userName).subscribe(member => {
+    this.memberService.getMember(this.user.userName).subscribe(member => {
       this.member = member;
+      
     })
   }
 
   updateMember(){
-    this.memberSecvice.updateMember(this.member).subscribe(() => {
+    this.memberService.updateMember(this.member).subscribe(() => {
       this.toastr.success('Profile updated successfully');
     this.editForm.reset(this.member);
     })
